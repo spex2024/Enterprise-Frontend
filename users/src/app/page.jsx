107eafe728d09fde494e23b281e1b9a11805b3dc
spex2024 <dercolbags@gmail.com>
2024-08-14 14:12:13 +0000

    v1.0.0

--- users/src/app/page.jsx
+++ users/src/app/page.jsx
@@ -4,7 +4,7 @@ import Menu from "@/app/components/page-ui/menu";
 import Header from "@/app/components/page-ui/header";
 import Cart from "@/app/components/page-ui/cart";
 
-export default function Home() {
+export default function App() {
   return (
     <main className="w-full flex min-h-screen flex-col items-center justify-between">
         <Header/>
