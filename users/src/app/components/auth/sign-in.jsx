75cf16d5ca69d3db51321102d7ba8bc4b8d9a5e8
spex2024 <dercolbags@gmail.com>
2024-08-14 10:45:48 +0000

    v1.0.0

--- users/src/app/components/auth/sign-in.jsx
+++ users/src/app/components/auth/sign-in.jsx
@@ -58,7 +58,7 @@ const SignIn = () => {
                     </form>
                     <div className="mt-7 w-full flex items-start  justify-start">
                         <p className="whitespace-nowrap text-gray-600 flex gap-4 w-full items-center">
-                            Don't have an account?
+                            Do not have an account?
                             <Link href={'/register'} className="underline-offset-4 font-semibold text-gray-900 underline">Sign up for free.</Link>
                         </p>
                     </div>
