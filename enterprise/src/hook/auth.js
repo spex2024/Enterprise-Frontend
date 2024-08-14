873c8c8cb6d4356a78527c7f32780a59af9507c1
spex2024 <dercolbags@gmail.com>
2024-08-14 17:02:00 +0000

    v1.0.0

--- enterprise/src/hook/auth.js
+++ enterprise/src/hook/auth.js
@@ -7,7 +7,8 @@ const useAuth = () => {
     const [success, setSuccess] = useState(null);
     const [error, setError] = useState(null);
     const router = useRouter();
-    const baseurl = 'http://localhost:8080';
+    const baseurl = 'https://enterprise-backend-l6pn.onrender.com';
+    // const baseurl = 'http://localhost:8080';
 
 
     const login = async (data) => {
