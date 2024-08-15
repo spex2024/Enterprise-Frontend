c01c29eb45a172f888679e4fdf693faeffadd69b
spex2024 <dercolbags@gmail.com>
2024-08-15 14:52:21 +0000

    v1.0.0.0

--- enterprise/src/app/page.tsx
+++ enterprise/src/app/page.tsx
@@ -1,25 +1,32 @@
 'use client'
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Dashboard from "@/components/page/dashboard";
 import useAuthStore from "@/store/authenticate";
 import { ScaleLoader } from "react-spinners";
+import { parseCookies } from 'nookies'; // Import nookies to handle cookies
 
 const App = () => {
-    const { isAuthenticated } = useAuthStore();
+    const { isAuthenticated, setIsAuthenticated } = useAuthStore();
     const router = useRouter();
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            if (!isAuthenticated) {
+        const checkAuthentication = () => {
+            const cookies = parseCookies(); // Get all cookies
+            const token = cookies.token; // Retrieve token from cookies
+             console.log(cookies.token);
+            if (token) {
+                // Here you would normally verify the token, but for simplicity, we assume it's valid
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
                 router.push('/login'); // Redirect to login page if not authenticated
             }
-        }, 1000); // Adjust the delay as needed
+        };
 
-        return () => clearTimeout(timer); // Clean up the timer if the component unmounts
-    }, [isAuthenticated, router]);
+        checkAuthentication();
+    }, [isAuthenticated, router, setIsAuthenticated]);
 
-    // Optionally, you can return a loading indicator while checking authentication
     if (!isAuthenticated) {
         return (
             <div className="flex items-center justify-center min-h-screen">
