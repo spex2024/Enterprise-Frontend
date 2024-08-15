60b09b85f7b07c27b6d0c1012372fb4a7a473d8a
spex2024 <dercolbags@gmail.com>
2024-08-15 17:09:07 +0000

    v1.0.0.0

--- enterprise/src/store/authenticate.ts
+++ enterprise/src/store/authenticate.ts
@@ -3,13 +3,51 @@ import create from 'zustand';
 interface AuthStore {
     isAuthenticated: boolean;
     setIsAuthenticated: (authState: boolean) => void;
+    checkAuthentication: () => boolean;
 }
 
+const EXPIRATION_TIME = 60 * 60 * 1000; // 1 hour in milliseconds
+
 const useAuthStore = create<AuthStore>((set) => ({
-    isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
-    setIsAuthenticated: (authState) => {
+    isAuthenticated: (() => {
+        const now = new Date().getTime();
+        const storedExpiration = localStorage.getItem('authExpiration');
+        const storedAuthState = localStorage.getItem('isAuthenticated');
+
+        if (storedAuthState === 'true' && storedExpiration) {
+            const expirationTime = parseInt(storedExpiration, 10);
+            if (now < expirationTime) {
+                return true;
+            } else {
+                localStorage.removeItem('isAuthenticated');
+                localStorage.removeItem('authExpiration');
+                return false;
+            }
+        }
+        return false;
+    })(),
+    setIsAuthenticated: (authState: boolean) => {
+        const now = new Date().getTime();
+        const expirationTime = now + EXPIRATION_TIME;
+
         set({ isAuthenticated: authState });
-        localStorage.setItem('isAuthenticated', authState.toString());
+
+        if (authState) {
+            localStorage.setItem('isAuthenticated', 'true');
+            localStorage.setItem('authExpiration', expirationTime.toString());
+        } else {
+            localStorage.removeItem('isAuthenticated');
+            localStorage.removeItem('authExpiration');
+        }
+    },
+    checkAuthentication: () => {
+        const now = new Date().getTime();
+        const storedExpiration = localStorage.getItem('authExpiration');
+        if (storedExpiration) {
+            const expirationTime = parseInt(storedExpiration, 10);
+            return now < expirationTime;
+        }
+        return false;
     },
 }));
 
