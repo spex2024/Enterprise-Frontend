e2c30a2528f5212f3ad2348fcbf24a1183e41710
spex2024 <dercolbags@gmail.com>
2024-08-18 18:34:26 +0000

    v1.0.0.0

--- employee/components/page-ui/card.jsx
+++ employee/components/page-ui/card.jsx
@@ -14,43 +14,26 @@ const FoodProductCard = () => {
     fetchMeals();
   }, [fetchMeals]);
 
-  // Extracting meals array from the meal object
-  const meals = meal?.meals || [];
-  const vendorName = meal?.vendorName || "";
-  const vendorLocation = meal?.vendorLocation || "";
- console.log(meal);
+  const meals = meal.flatMap((item) => item.meals);
+
   return (
     <div className="w-full grid grid-cols-1 lg:grid-cols-4 justify-center lg:px-10 place-items-center px-5">
-      {meals.map((mealItem) => (
-        <div
-          key={mealItem._id}
-          className="group my-10 w-full lg:max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md m-4"
-        >
-          <Link
-            className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-            href="#"
-          >
-            <img
-              alt="mealimage"
-              className="peer absolute top-0 right-0 h-full w-full object-cover"
-              src={mealItem.imageUrl}
-            />
+      {meals?.map((meal) => (
+        <div key={meal._id} className="group my-10 w-full lg:max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md m-4">
+          <Link className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+            <img className="peer absolute top-0 right-0 h-full w-full object-cover" src={meal?.imageUrl} alt="mealimage" />
           </Link>
           <div className="mt-4 px-5 pb-5">
             <div className="mt-2 mb-5 flex items-center justify-between">
               <Link href="#">
-                <h5 className="text-2xl tracking-tight text-slate-900 font-bold">
-                  {mealItem.main?.name || ""}
-                </h5>
+                <h5 className="text-2xl tracking-tight text-slate-900 font-bold">{meal.main?.name || ''}</h5>
               </Link>
-              <button onClick={() => openModal(mealItem)}>
-                <ShoppingBasket className="text-black" />
+              <button onClick={() => openModal(meal)}>
+                <ShoppingBasket className="text-black"/>
               </button>
             </div>
             <div className="w-full flex items-center justify-between rounded-full text-center text-sm cursor-pointer">
-              <p className="text-xs">
-                {vendorName} ({vendorLocation})
-              </p>
+              <p className={`text-xs`}>{meal.vendor.name}({meal.vendor.location})</p>
             </div>
           </div>
         </div>
