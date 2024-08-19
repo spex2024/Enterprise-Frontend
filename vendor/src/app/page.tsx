91892b1de28796212333d75c7b55520ef39791b5
spex2024 <dercolbags@gmail.com>
2024-08-19 14:13:09 +0000

    v1.0.0.0

--- vendor/src/app/page.tsx
+++ vendor/src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Dashboard() {
         <div className="flex min-h-screen w-full flex-col">
             <Header />
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-                <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
                     <Card x-chunk="dashboard-01-chunk-0">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">Total Orders</CardTitle>
