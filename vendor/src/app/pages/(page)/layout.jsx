76f98ea81bcb880726fa663a6fe450f1ba7fbcd3
spex2024 <dercolbags@gmail.com>
2024-08-22 09:27:04 +0000

    v1.0.0.0

--- vendor/src/app/pages/(page)/layout.jsx
+++ vendor/src/app/pages/(page)/layout.jsx
@@ -1,16 +1,14 @@
 import Header from "../../../components/main/header";
 import {VendorProvider} from "../../store/vendor";
-import {MealProvider} from "../../store/meal";
 
 
 export default function Layout({ children }) {
     return (
         <>
             <VendorProvider>
-                <MealProvider>
+
              <Header/>
             <main>{children}</main>
-              </MealProvider>
             </VendorProvider>
         </>
 
