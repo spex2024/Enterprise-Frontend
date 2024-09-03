5e76980f6ad1e9105667dc5289a798af0af22556
spex2024 <dercolbags@gmail.com>
2024-09-03 16:37:42 +0000

    v1.0.0.0

--- enterprise/src/hook/auth.js
+++ enterprise/src/hook/auth.js
@@ -11,7 +11,8 @@ const useAuth = () => {
     const router = useRouter();
     const { setIsAuthenticated ,logout: clearAuth} = useAuthStore()
     const {  fetchUser } = useUserStore()
-    const baseurl = 'https://enterprise-backend.vercel.app';
+    // const baseurl = 'https://enterprise-backend.vercel.app';
+    const baseurl = 'https://enterprise-backend-l6pn.onrender.com';
     // const baseurl = 'http://localhost:8080';
 
     const login = async (data) => {
--- enterprise/src/store/profile.ts
+++ enterprise/src/store/profile.ts
@@ -2,7 +2,8 @@
 import {create} from 'zustand';
 import axios from 'axios';
 
- const baseurl = 'https://enterprise-backend.vercel.app';
+ // const baseurl = 'https://enterprise-backend.vercel.app';
+ const baseurl = 'https://enterprise-backend-l6pn.onrender.com';
 // const baseurl = 'http://localhost:8080';
 
 interface User {
